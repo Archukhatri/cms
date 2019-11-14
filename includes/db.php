2ab980e2d058ecef6e75317e649f2bdf717145cb
archu <archukhatri987@gmail.com>
2019-11-14 16:04:31 +0545

    hht

--- includes/db.php
+++ includes/db.php
@@ -11,7 +11,7 @@ foreach($db as $key => $value){
 
 $connection = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
 
-if($connection){
-	echo "we are conected";
-}
+// if($connection){
+// 	echo "we are conected";
+// }
 ?>
--- search.php
+++ search.php
@@ -22,10 +22,38 @@
             <!-- Blog Entries Column -->
             <div class="col-md-8">
                 <?php
-                  $query ="SELECT * FROM posts";
-                   $select_all_posts_query = mysqli_query($connection,$query);
 
-                    while($row = mysqli_fetch_assoc($select_all_posts_query)){//loop start here
+             if(isset($_POST['submit'])){
+
+               $search = $_POST['search'];
+
+               $query = "SELECT * FROM posts WHERE post_tags LIKE '%$search%' ";
+
+               $search_query = mysqli_query($connection, $query);
+
+               if(!$search_query){
+
+                die("QUERY FAILED" . mysqli_error($connection));
+
+               }
+
+               $count = mysqli_num_rows($search_query);
+
+               if($count == 0){
+                echo "<h1> NO RESULT </h1>";
+               }else {
+                echo "SOME RESULT";
+              }
+
+             }
+
+
+
+            
+                  // $query ="SELECT * FROM posts";
+                  //  $select_all_posts_query = mysqli_query($connection,$query);
+
+                    while($row = mysqli_fetch_assoc($search_query)){//loop start here
                         $post_title=$row['post_title'];
                         $post_author=$row['post_author'];
                         $post_date=$row['post_date'];
